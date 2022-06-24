99bbd5099da1b08a17443ae8235edcbf14793fe1
Carlos Sabado Cortes <carlos.sabado.cortes@everis.nttdata.com>
2022-06-24 15:36:41 +0100

    zoom video web app

---
+++ src/config/dev.ts
@@ -0,0 +1,10 @@
+import { getExploreName } from '../utils/platform';
+
+export const devConfig = {
+  sdkKey: '',
+  sdkSecret: '',
+  topic: 'test',
+  name: 'user',
+  password: 'user123',
+  signature: ''
+};
